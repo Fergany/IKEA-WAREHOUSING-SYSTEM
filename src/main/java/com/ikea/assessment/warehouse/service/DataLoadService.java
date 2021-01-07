4766aaceadd64e85a59e75cbc14db41306c390d0
Hazem <hfergany@gmail.com>
2021-01-07 09:28:37 +0100

    add DataLoadService implementation class

---
+++ src/main/java/com/ikea/assessment/warehouse/service/DataLoadService.java
@@ -0,0 +1,12 @@
+package com.ikea.assessment.warehouse.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class DataLoadService implements IDataLoadService {
+
+    @Override
+    public void loadData(String inventoryFilePath, String productsFilePath) {
+
+    }
+}
