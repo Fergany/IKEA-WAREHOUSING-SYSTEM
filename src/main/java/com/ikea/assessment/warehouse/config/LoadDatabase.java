f7e4e1933c50f71b7dfd15f40322bae6ebb2ee09
Hazem <hfergany@gmail.com>
2021-01-07 14:12:00 +0100

    prevent CommandLineRunner bean to run in case profile:test

--- src/main/java/com/ikea/assessment/warehouse/config/LoadDatabase.java
+++ src/main/java/com/ikea/assessment/warehouse/config/LoadDatabase.java
@@ -5,7 +5,9 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
+@Profile("!test")
 @Configuration
 public class LoadDatabase {
 
