fc1608fcd847b1e29e98ca0f9aac033f087af43a
Hazem <hfergany@gmail.com>
2021-01-07 14:43:28 +0100

    implement SpringFoxConfig.java

--- src/main/java/com/ikea/assessment/warehouse/configuration/SpringFoxConfig.java
+++ src/main/java/com/ikea/assessment/warehouse/configuration/SpringFoxConfig.java
@@ -1,20 +1,51 @@
 package com.ikea.assessment.warehouse.configuration;
 
+import com.google.common.base.Predicate;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import springfox.documentation.RequestHandler;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
-public class SpringFoxConfig {
+@EnableSwagger2
+class SwaggerFoxConfig  implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+
+        registry
+                .addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+        registry
+                .addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.any())
+                .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
                 .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo getApiInfo() {
+
+        return new ApiInfoBuilder()
+                .title("IKEA Warehousing System")
+                .description("Implementing a simple warehouse system to manage the products and articles")
+                .version("1.0.0")
                 .build();
     }
 }
