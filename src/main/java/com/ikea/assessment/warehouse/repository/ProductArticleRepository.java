ce008a72b08ae355dc68b1fd010f303b84206d58
Hazem <hfergany@gmail.com>
2021-01-07 10:57:37 +0100

    correct Generic type

--- src/main/java/com/ikea/assessment/warehouse/repository/ProductArticleRepository.java
+++ src/main/java/com/ikea/assessment/warehouse/repository/ProductArticleRepository.java
@@ -1,5 +1,6 @@
 package com.ikea.assessment.warehouse.repository;
 
+import com.ikea.assessment.warehouse.entity.ProductArticle;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
