9a639632dd4d14340bc75bd0c9725405fafc2cce
Hazem <hfergany@gmail.com>
2021-01-07 12:49:47 +0100

    add findByProductAndStatus

--- src/main/java/com/ikea/assessment/warehouse/repository/ProductArticleRepository.java
+++ src/main/java/com/ikea/assessment/warehouse/repository/ProductArticleRepository.java
@@ -1,9 +1,15 @@
 package com.ikea.assessment.warehouse.repository;
 
+import com.ikea.assessment.warehouse.entity.Product;
 import com.ikea.assessment.warehouse.entity.ProductArticle;
+import com.ikea.assessment.warehouse.entity.ProductStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface ProductArticleRepository extends JpaRepository<ProductArticle, Long> {
+    public Optional<List<ProductArticle>> findByProductAndStatus(Product product, ProductStatus status);
 }
