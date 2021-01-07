11db0642f767e6538aa7ce2f8fc50f475a9c4be2
Hazem <hfergany@gmail.com>
2021-01-07 12:51:28 +0100

    add convertToDTO

--- src/main/java/com/ikea/assessment/warehouse/mapper/ArticleMapper.java
+++ src/main/java/com/ikea/assessment/warehouse/mapper/ArticleMapper.java
@@ -1,5 +1,6 @@
 package com.ikea.assessment.warehouse.mapper;
 
+import com.ikea.assessment.warehouse.dto.ArticleDTO;
 import com.ikea.assessment.warehouse.entity.Article;
 import org.json.simple.JSONObject;
 
@@ -11,6 +12,13 @@ public class ArticleMapper {
         long stock = Long.parseLong((String) articleJSONObject.get("stock"));
 
         return new Article(id, name, stock);
+    }
+
+    public static ArticleDTO convertToDTO(Article article){
+        String art_id = String.valueOf( article.getId());
+        String amount_of = String.valueOf( article.getStock());
+
+        return new ArticleDTO(art_id, amount_of);
     }
 
 }
