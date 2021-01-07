2caf98060579b8ee6ed2d36d473159178ff25f07
Hazem <hfergany@gmail.com>
2021-01-07 12:33:09 +0100

    add IProductService

--- src/main/java/com/ikea/assessment/warehouse/service/IProductService.java
+++ src/main/java/com/ikea/assessment/warehouse/service/IProductService.java
@@ -1,5 +1,9 @@
 package com.ikea.assessment.warehouse.service;
 
+import com.ikea.assessment.warehouse.dto.ProductDTO;
+
+import java.util.List;
+
 public interface IProductService {
     public List<ProductDTO> getAll();
     public void sell(long id);
