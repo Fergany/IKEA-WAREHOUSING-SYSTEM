4f300d9d01ce74c1adaf8be4fa422ff18f3308df
Hazem <hfergany@gmail.com>
2021-01-07 10:12:24 +0100

    add ProductRepository

---
+++ src/main/java/com/ikea/assessment/warehouse/repository/ProductArticle.java
@@ -0,0 +1,9 @@
+package com.ikea.assessment.warehouse.repository;
+
+import com.ikea.assessment.warehouse.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductArticle extends JpaRepository<Product, Long> {
+}
