79bce5c77c1075377762a26175b1e9e54c5abec9
Hazem <hfergany@gmail.com>
2021-01-07 09:42:59 +0100

    add Product entity

---
+++ src/main/java/com/ikea/assessment/warehouse/entity/Product.java
@@ -0,0 +1,32 @@
+package com.ikea.assessment.warehouse.entity;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "products")
+@NoArgsConstructor
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "product_id")
+    private long id;
+
+    @Column(name = "product_name")
+    private String name;
+
+    @Column(name = "product_status")
+    private ProductStatus status;
+
+    public Product(String name) {
+        this.name = name;
+    }
+
+    public Product(String name, ProductStatus status) {
+        this.name = name;
+        this.status = status;
+    }
+
+}
