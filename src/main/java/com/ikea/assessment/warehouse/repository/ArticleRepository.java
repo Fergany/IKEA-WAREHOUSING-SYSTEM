69150c97bdc08e97f47e56dde60a4d744cd2fa91
Hazem <hfergany@gmail.com>
2021-01-07 10:11:20 +0100

    add ArticleRepository

---
+++ src/main/java/com/ikea/assessment/warehouse/repository/ArticleRepository.java
@@ -0,0 +1,9 @@
+package com.ikea.assessment.warehouse.repository;
+
+import com.ikea.assessment.warehouse.entity.Article;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ArticleRepository extends JpaRepository<Article, Long> {
+}
