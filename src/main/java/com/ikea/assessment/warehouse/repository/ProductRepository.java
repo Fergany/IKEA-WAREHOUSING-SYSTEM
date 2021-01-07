d1e89c48c06efd4c68ac4aaad977f3549d4e3d03
Hazem <hfergany@gmail.com>
2021-01-07 12:47:16 +0100

    add findAllByStatus

--- src/main/java/com/ikea/assessment/warehouse/repository/ProductRepository.java
+++ src/main/java/com/ikea/assessment/warehouse/repository/ProductRepository.java
@@ -1,9 +1,13 @@
 package com.ikea.assessment.warehouse.repository;
 
 import com.ikea.assessment.warehouse.entity.Product;
+import com.ikea.assessment.warehouse.entity.ProductStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    public List<Product> findAllByStatus(ProductStatus status);
 }
