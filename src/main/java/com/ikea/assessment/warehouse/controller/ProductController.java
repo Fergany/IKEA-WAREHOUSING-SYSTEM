270a22273280e4f99faddd9048f57aa605f944f7
Hazem <hfergany@gmail.com>
2021-01-07 12:33:42 +0100

    add ProductController

---
+++ src/main/java/com/ikea/assessment/warehouse/controller/ProductController.java
@@ -0,0 +1,29 @@
+package com.ikea.assessment.warehouse.controller;
+
+import com.ikea.assessment.warehouse.dto.ProductDTO;
+import com.ikea.assessment.warehouse.service.IProductService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/products")
+public class ProductController {
+    private final IProductService productService;
+
+    public ProductController(IProductService productService){
+        this.productService = productService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ProductDTO>> getAll(){
+        return new ResponseEntity<List<ProductDTO>>(productService.getAll(), HttpStatus.OK);
+    }
+
+    @PutMapping("/{id}/sell")
+    public ResponseEntity sell(@PathVariable long id){
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
+    }
+}
