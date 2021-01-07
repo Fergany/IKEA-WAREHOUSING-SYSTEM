34162bc39879d698be4f7988156f5393cba1c889
Hazem <hfergany@gmail.com>
2021-01-07 14:43:54 +0100

    add swagger docs to ProductController

--- src/main/java/com/ikea/assessment/warehouse/controller/ProductController.java
+++ src/main/java/com/ikea/assessment/warehouse/controller/ProductController.java
@@ -2,6 +2,8 @@ package com.ikea.assessment.warehouse.controller;
 
 import com.ikea.assessment.warehouse.dto.ProductDTO;
 import com.ikea.assessment.warehouse.service.IProductService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@Api(tags = {"Products API"})
 @RequestMapping("/api/v1/products")
 public class ProductController {
     private final IProductService productService;
@@ -17,11 +20,13 @@ public class ProductController {
         this.productService = productService;
     }
 
+    @ApiOperation(value = "View a list of NEW products")
     @GetMapping
     public ResponseEntity<List<ProductDTO>> getAllNew(){
         return new ResponseEntity<List<ProductDTO>>(productService.getNewProducts(), HttpStatus.OK);
     }
 
+    @ApiOperation(value = "Sell Product")
     @PutMapping("/{id}/sell")
     public ResponseEntity sell(@PathVariable long id){
         return new ResponseEntity(HttpStatus.NO_CONTENT);
