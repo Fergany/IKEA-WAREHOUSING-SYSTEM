58503999d7d469867e8e054123f4f85b50933e7a
Hazem <hfergany@gmail.com>
2021-01-07 10:23:46 +0100

    add DataLoadException

---
+++ src/main/java/com/ikea/assessment/warehouse/exception/DataLoadException.java
@@ -0,0 +1,7 @@
+package com.ikea.assessment.warehouse.exception;
+
+public class DataLoadException extends RuntimeException {
+    public DataLoadException(String message) {
+        super(message);
+    }
+}
