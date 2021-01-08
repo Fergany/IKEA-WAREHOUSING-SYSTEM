dfaa15d92f0c0a72abb96ce14221a7f97ae9f618
Hazem <hfergany@gmail.com>
2021-01-08 20:16:38 +0100

    add logger to Exception handler

--- src/main/java/com/ikea/assessment/warehouse/advice/ExceptionHandlerAdvice.java
+++ src/main/java/com/ikea/assessment/warehouse/advice/ExceptionHandlerAdvice.java
@@ -3,6 +3,9 @@ package com.ikea.assessment.warehouse.advice;
 import com.ikea.assessment.warehouse.exception.DataLoadException;
 import com.ikea.assessment.warehouse.exception.InsufficientStockException;
 import com.ikea.assessment.warehouse.exception.ObjectNotFoundException;
+import com.ikea.assessment.warehouse.service.ProductServiceImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -11,23 +14,29 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class ExceptionHandlerAdvice {
 
+    Logger logger = LoggerFactory.getLogger(ExceptionHandlerAdvice.class);
+
     @ExceptionHandler(DataLoadException.class)
     ResponseEntity<DataLoadException> dataLoadExceptionHandler(DataLoadException exception) {
+        logger.error(exception.getMessage());
         return new ResponseEntity<DataLoadException>(exception, HttpStatus.NOT_ACCEPTABLE);
     }
 
     @ExceptionHandler(ObjectNotFoundException.class)
     ResponseEntity<ObjectNotFoundException> objectNotFoundExceptionHandler(ObjectNotFoundException exception) {
+        logger.error(exception.getMessage());
         return new ResponseEntity<ObjectNotFoundException>(exception, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(InsufficientStockException.class)
     ResponseEntity<InsufficientStockException> insufficientStockExceptionHandler(InsufficientStockException exception) {
+        logger.error(exception.getMessage());
         return new ResponseEntity<InsufficientStockException>(exception, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(RuntimeException.class)
     ResponseEntity<RuntimeException> runTimeExceptionHandler(RuntimeException exception) {
+        logger.error(exception.getMessage());
         return new ResponseEntity<RuntimeException>(exception, HttpStatus.BAD_REQUEST);
     }
 
