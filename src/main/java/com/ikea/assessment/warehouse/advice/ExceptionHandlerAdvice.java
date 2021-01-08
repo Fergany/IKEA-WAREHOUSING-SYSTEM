8320c81fdf7612224b93b9ff1038730ce2d0b8e2
Hazem <hfergany@gmail.com>
2021-01-08 08:15:21 +0100

    add objectNotFoundExceptionHandler()

--- src/main/java/com/ikea/assessment/warehouse/advice/ExceptionHandlerAdvice.java
+++ src/main/java/com/ikea/assessment/warehouse/advice/ExceptionHandlerAdvice.java
@@ -1,6 +1,7 @@
 package com.ikea.assessment.warehouse.advice;
 
 import com.ikea.assessment.warehouse.exception.DataLoadException;
+import com.ikea.assessment.warehouse.exception.ObjectNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -12,6 +13,11 @@ public class ExceptionHandlerAdvice {
     @ExceptionHandler(DataLoadException.class)
     ResponseEntity<DataLoadException> dataLoadExceptionHandler(DataLoadException exception) {
         return new ResponseEntity<DataLoadException>(exception, HttpStatus.NOT_ACCEPTABLE);
+    }
+
+    @ExceptionHandler(ObjectNotFoundException.class)
+    ResponseEntity<ObjectNotFoundException> objectNotFoundExceptionHandler(ObjectNotFoundException exception) {
+        return new ResponseEntity<ObjectNotFoundException>(exception, HttpStatus.NOT_FOUND);
     }
 
 }
