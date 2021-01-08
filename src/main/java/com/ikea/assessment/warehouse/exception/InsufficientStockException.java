3f14dc489b896f15f695153a1bf2783e441fdcc4
Hazem <hfergany@gmail.com>
2021-01-08 11:57:37 +0100

    edit constructor

--- src/main/java/com/ikea/assessment/warehouse/exception/InsufficientStockException.java
+++ src/main/java/com/ikea/assessment/warehouse/exception/InsufficientStockException.java
@@ -1,7 +1,7 @@
 package com.ikea.assessment.warehouse.exception;
 
 public class InsufficientStockException extends RuntimeException {
-    public InsufficientStockException(long artcileId){
-        super("Article " + artcileId + " don't have sufficient stock for this product.");
+    public InsufficientStockException(long artcileId, long productId){
+        super("Article " + artcileId + " don't have sufficient stock for this product " + productId);
     }
 }
